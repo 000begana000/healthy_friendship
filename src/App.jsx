3f12043d60bd22a8de1807e0ec8d45322a5b4cab
Begana <begana.contact@gmail.com>
2025-02-06 16:32:21 +0100

    feat: create new buttons & style

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
+// import { FREINDSHIPS } from "./data.js";
+
 import Header from "./components/Header/Header";
+import TabButton from "./components/UI/TabButton";
 
 export default function App() {
   return (
@@ -7,6 +10,12 @@ export default function App() {
       <div className="circle-2"></div>
       <div className="container">
         <Header />
+        <main className="examples">
+          <TabButton>trust</TabButton>
+          <TabButton>royalty</TabButton>
+          <TabButton>connection</TabButton>
+          <TabButton>support</TabButton>
+        </main>
       </div>
     </>
   );
