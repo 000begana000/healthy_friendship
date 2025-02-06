e00f329c5744ce0edb8185d28bed48e14589e919
Begana <begana.contact@gmail.com>
2025-02-06 15:16:59 +0100

    feat: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,15 @@ import heroImg from "../assets/hero.png";
 
 export default function Header() {
   return (
-    <header>
-      <h1>discover healthy relationship and boundary with friendship</h1>
-      <p>
-        A true friendship is effortless yet meaningful, where both individuals
-        feel valued and understood. It thrives on sincerity, loyalty, and shared
-        moments of joy and growth.{" "}
-      </p>
+    <header className="header">
+      <div className="heading">
+        <h1>discover healthy relationship and boundary with friendship</h1>
+        <p>
+          A true friendship is effortless yet meaningful, where both individuals
+          feel valued and understood. It thrives on sincerity, loyalty, and
+          shared moments of joy and growth.{" "}
+        </p>
+      </div>
       <img src={heroImg} alt="a girl drawing" />
     </header>
   );
