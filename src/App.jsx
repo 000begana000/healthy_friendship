d95d7e452508f6de4a919a9ba5b87b3931fb8e25
Begana <begana.contact@gmail.com>
2025-02-08 15:04:44 +0100

    fix: adjust background style

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Examples from "./components/Examples";
 export default function App() {
   return (
     <>
-      <div className="circle-1"></div>
-      <div className="circle-2"></div>
       <div className="container">
         <Header />
         <Examples />
