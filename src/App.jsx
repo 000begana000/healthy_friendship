e9ec652f6a240a3027b1421f6b57efa5a63b9086
Begana <begana.contact@gmail.com>
2025-02-08 14:38:34 +0100

    fix: create Examples component

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,14 @@
-import { FREINDSHIPS } from "./data.js";
-import { useState } from "react";
-
 import Header from "./components/Header/Header";
-import TabButton from "./components/UI/TabButton";
+import Examples from "./components/Examples";
 
 export default function App() {
-  const [selectedTopic, setSelectedTopic] = useState("trust");
-
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-  }
-
   return (
     <>
       <div className="circle-1"></div>
       <div className="circle-2"></div>
       <div className="container">
         <Header />
-        <main className="examples">
-          <TabButton
-            isSelected={selectedTopic === "trust"}
-            onSelect={() => handleSelect("trust")}
-          >
-            trust
-          </TabButton>
-          <TabButton
-            isSelected={selectedTopic === "royalty"}
-            onSelect={() => handleSelect("royalty")}
-          >
-            royalty
-          </TabButton>
-          <TabButton
-            isSelected={selectedTopic === "connection"}
-            onSelect={() => handleSelect("connection")}
-          >
-            connection
-          </TabButton>
-          <TabButton
-            isSelected={selectedTopic === "support"}
-            onSelect={() => handleSelect("support")}
-          >
-            support
-          </TabButton>
-        </main>
-        <div className="card">
-          <div className="content">
-            <img src={FREINDSHIPS[selectedTopic].img} alt="" />
-            <p>{FREINDSHIPS[selectedTopic].description}</p>
-          </div>
-        </div>
+        <Examples />
       </div>
     </>
   );
---
+++ src/components/Examples.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+
+import TabButton from "./UI/TabButton";
+
+import { FREINDSHIPS } from "../data.js";
+
+export default function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState("trust");
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+  return (
+    <>
+      <main className="examples">
+        <TabButton
+          isSelected={selectedTopic === "trust"}
+          onSelect={() => handleSelect("trust")}
+        >
+          trust
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "royalty"}
+          onSelect={() => handleSelect("royalty")}
+        >
+          royalty
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "connection"}
+          onSelect={() => handleSelect("connection")}
+        >
+          connection
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "support"}
+          onSelect={() => handleSelect("support")}
+        >
+          support
+        </TabButton>
+      </main>
+      <div className="card">
+        <div className="content">
+          <img src={FREINDSHIPS[selectedTopic].img} alt="" />
+          <p>{FREINDSHIPS[selectedTopic].description}</p>
+        </div>
+      </div>
+    </>
+  );
+}
